a67a9f08c60da6f0383940944704c3d2a13a33f3
vineetkumar7830 <vineetbhardwaj7830@gmail.com>
2025-12-08 13:23:18 +0530

    change URL

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,9 +1,9 @@
 // src/config.js
 const config = {
-  BASE_URL: "https://www.cakistockmarket.com/api/v1/",
+  // BASE_URL: "https://www.cakistockmarket.com/api/v1/",
+    BASE_URL: "http://cakistockmarket.com/makemytrade-api/api/v1",
   IMAGE_URL: "https://www.cakistockmarket.com/",
   
-
 };
 
 export default config;
